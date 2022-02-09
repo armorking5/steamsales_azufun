b3b3eb702425a0aecddf94c0ad667aa19f9b1724
Stefano Africano <stefano.africano@ol3online.com>
2022-02-09 17:45:13 +0100

    inizio classe steam wishlist class

--- GetWishlistData.cs
+++ GetWishlistData.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -8,10 +9,22 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
+
 namespace SteamSalesApp
 {
+
+	public class SteamWishlist
+	{
+
+
+
+	}
+
 	public static class GetWishlistData
 	{
+
+		static readonly HttpClient client = new HttpClient();
+
 		[FunctionName("GetWishlistData")]
 		public static async Task<IActionResult> Run(
 			[HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
@@ -21,16 +34,37 @@ namespace SteamSalesApp
 
 			// https://store.steampowered.com/wishlist/profiles/76561198033429344/wishlistdata/
 			string name = req.Query["name"];
+			string steamId = req.Query["steamId"];
+			steamId = steamId ?? "76561198033429344";
 
-			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-			dynamic data = JsonConvert.DeserializeObject(requestBody);
-			name = name ?? data?.name;
+			// Call asynchronous network methods in a try/catch block to handle exceptions.
+			try
+			{
+				string uri = "https://store.steampowered.com/wishlist/profiles/" + steamId + "/wishlistdata/";
+				// HttpResponseMessage response = await client.GetAsync("https://store.steampowered.com/wishlist/profiles/" + steamId + "/wishlistdata/");
+				// response.EnsureSuccessStatusCode();
+				// string responseBody = await response.Content.ReadAsStringAsync();
+				// Above three lines can be replaced with new helper method below
+				string responseBody = await client.GetStringAsync(uri);
+				dynamic json = JsonConvert.DeserializeObject<SteamWishlist>(responseBody);
+				return new OkObjectResult(json);
+			}
+			catch (HttpRequestException e)
+			{
+				log.LogCritical("\nException Caught!");
+				log.LogCritical("Message :{0} ", e.Message);
+				return new NotFoundObjectResult("Steam problems");
+			}
 
-			string responseMessage = string.IsNullOrEmpty(name)
-				? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-				: $"Hello, {name}. This HTTP triggered function executed successfully.";
+			// string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+			// dynamic data = JsonConvert.DeserializeObject(requestBody);
+			// name = name ?? data?.name;
 
-			return new OkObjectResult(responseMessage);
+			// string responseMessage = string.IsNullOrEmpty(name)
+			// 	? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+			// 	: $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+			// return new OkObjectResult(responseMessage);
 		}
 	}
 }
